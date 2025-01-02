65163c85f1945b5c1ac037f7aab71bea328fdf58
DESKTOP-7RQ494B\HeoSeonJin <sonjin54@naver.com>
2025-01-02 23:38:00 +0900

    http로 서버 구동하기

--- node/login-lecture/app.js
+++ node/login-lecture/app.js
@@ -1,3 +1,19 @@
+const http = require("http"); // http는 내장 모듈
+// http는 if문으로 url 처리도 해줘야하고, 한글 처리까지 해줘야하기 때문에 express를 쓴다.
+const app = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" }); // 한글 처리
+  if (req.url === "/") {
+    res.end("여기는 루트입니다");
+  } else if (req.url === "/login") {
+    res.end("여기는 로그인 화면입니다.");
+  }
+});
+
+app.listen(3001, () => {
+  console.log("http로 서버 가동");
+});
+
+/* express 모듈을 사용한 서버 구동동
 // 서버의 중심 파일 - express 프레임워크
 const express = require("express");
 const app = express();
@@ -18,3 +34,4 @@ app.listen(3000, function () {
 // npm i express --save 로 모듈 설치
 // node app.js로 서버 가동
 // localhost:3000으로 접속하여 서버가 가동 되는지 확인
+*/
