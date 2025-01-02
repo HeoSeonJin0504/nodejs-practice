e36ac51e41a00a496e75c6d067a6a7f185f7c0de
DESKTOP-7RQ494B\HeoSeonJin <sonjin54@naver.com>
2025-01-03 06:19:34 +0900

    MVC의 C(컨트롤러) 분리하기

--- node/login-lecture/routes/home/index.js
+++ node/login-lecture/routes/home/index.js
@@ -3,13 +3,10 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  // 브라우저에서 루트 경로로 요청이 들어왔을 때 실행할 코드
-  res.render("home/index");
-});
+const ctrl = require("./home.ctrl");
 
-router.get("/login", (req, res) => {
-  res.render("home/login");
-});
+router.get("/", ctrl.home);
+
+router.get("/login", ctrl.login);
 
 module.exports = router; // 외부에서 사용 가능하게 (app.js에서 사용이 가능해야하니까)
