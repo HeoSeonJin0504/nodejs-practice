6509c0bd202970763169a87c0833b7a44d5ea5ca
DESKTOP-7RQ494B\HeoSeonJin <sonjin54@naver.com>
2025-01-02 23:48:31 +0900

    로그인 및 루트 화면 코드 작성(하드코딩)

--- node/login-lecture/app.js
+++ node/login-lecture/app.js
@@ -1,3 +1,4 @@
+/* http 모듈을 사용한 서버 구동
 const http = require("http"); // http는 내장 모듈
 // http는 if문으로 url 처리도 해줘야하고, 한글 처리까지 해줘야하기 때문에 express를 쓴다.
 const app = http.createServer((req, res) => {
@@ -12,19 +13,46 @@ const app = http.createServer((req, res) => {
 app.listen(3001, () => {
   console.log("http로 서버 가동");
 });
+*/
 
-/* express 모듈을 사용한 서버 구동동
+/* express 모듈을 사용한 서버 구동 */
 // 서버의 중심 파일 - express 프레임워크
 const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
   // 브라우저에서 루트 경로로 요청이 들어왔을 때 실행할 코드
-  res.send("여기는 루트입니다.");
+  res.send(`
+    <!DOCTYPE html>
+    <html lang="ko">
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Document</title>
+    </head>
+    <body>
+      여기는 루트입니다.
+    </body>
+    </html>
+  `);
 });
 
 app.get("/login", (req, res) => {
-  res.send("여기는 로그인 화면입니다.");
+  res.send(`
+    <!DOCTYPE html>
+    <html lang="ko">
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Document</title>
+    </head>
+    <body>
+      <input type="text" placeholder = "아이디"><br>
+      <input type="text" placeholder = "비밀번호"><br>
+      <button>로그인</button>
+    </body>
+    </html>
+  `);
 });
 
 app.listen(3000, function () {
@@ -34,4 +62,3 @@ app.listen(3000, function () {
 // npm i express --save 로 모듈 설치
 // node app.js로 서버 가동
 // localhost:3000으로 접속하여 서버가 가동 되는지 확인
-*/
