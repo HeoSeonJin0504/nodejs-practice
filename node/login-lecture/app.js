c84576c1cc4d537bd013775d5a114f84d19821c6
DESKTOP-7RQ494B\HeoSeonJin <sonjin54@naver.com>
2025-01-05 19:55:39 +0900

    pacakage.json | node_modules

--- node/login-lecture/app.js
+++ node/login-lecture/app.js
@@ -1,6 +1,10 @@
 /* express 모듈을 사용한 서버 구동 */
 // 서버의 중심 파일 - express 프레임워크
 
+// npm i express --save 로 모듈 설치
+// node app.js로 서버 가동
+// localhost:3000으로 접속하여 서버가 가동 되는지 확인
+
 //모듈
 const express = require("express");
 const app = express();
@@ -17,6 +21,3 @@ app.use("/", home); //  use - 미들웨어를 등록해주는 메서드
 
 module.exports = app;
 
-// npm i express --save 로 모듈 설치
-// node app.js로 서버 가동
-// localhost:3000으로 접속하여 서버가 가동 되는지 확인
