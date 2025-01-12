497e84ae5fb27930bedc228543ecfd3e6802e2b0
DESKTOP-7RQ494B\HeoSeonJin <sonjin54@naver.com>
2025-01-12 23:45:50 +0900

    User 모델 만들기 | 인스턴스화

--- node/login-lecture/app/src/models/UserStorage.js
+++ node/login-lecture/app/src/models/UserStorage.js
@@ -2,7 +2,8 @@
 
 // UserStorage 클래스 생성 (클래스 안에 객체는 const 안쓴다)
 class UserStorage {
-  static #users = { // static으로 선언하면 클래스명으로 접근 가능, # = private
+  static #users = {
+    // static으로 선언하면 클래스명으로 접근 가능, # = private
     id: ["test", "test1"],
     password: ["1234", "12345"],
     name: ["테스트", "테스트1"],
@@ -12,12 +13,24 @@ class UserStorage {
     const users = this.#users;
     const newUsers = fields.reduce((newUsers, field) => {
       console.log(newUsers, field);
-      if (users.hasOwnProperty(field)) { // users에 field가 존재하면
+      if (users.hasOwnProperty(field)) {
+        // users에 field가 존재하면
         newUsers[field] = users[field]; // newUsers에 field 추가
       }
       return newUsers;
     }, {});
     return newUsers;
+  }
+
+  static getUserInfo(id) {
+    const users = this.#users;
+    const idx = users.id.indexOf(id);
+    const usersKeys = Object.keys(users); // users의 key값을 배열로 반환 => [id, password, name]
+    const userInfo = usersKeys.reduce((newUser, info) => {
+      newUser[info] = users[info][idx];
+      return newUser;
+    }, {});
+    return userInfo;
   }
 }
 module.exports = UserStorage;
--- node/login-lecture/app/src/routes/home/home.ctrl.js
+++ node/login-lecture/app/src/routes/home/home.ctrl.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const UserStorage = require("../../models/UserStorage"); // UserStorage 클래스 가져오기
+const User = require("../../models/User"); // User 클래스 가져오기
 
 // get 방식으로 접속했을 때, 렌더링 할 수 있게 오브젝트로 생성
 const output = {
@@ -14,20 +15,8 @@ const output = {
 
 const process = {
   login: (req, res) => {
-    const id = req.body.id,
-      password = req.body.password;
-
-    const users = UserStorage.getUsers("id", "password");
-    const response = {}; // 응답 객체
-    if (users.id.includes(id)) {
-      const idx = users.id.indexOf(id);
-      if (users.password[idx] === password) {
-        response.success = true;
-        return res.json(response);
-      }
-    }
-    response.success = false;
-    response.msg = "로그인에 실패하였습니다.";
+    const user = new User(req.body);
+    const response = user.login();
     return res.json(response);
   },
 };
