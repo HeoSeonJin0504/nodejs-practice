59379850d8a5ae3378ff14e7f12c795f13249942
DESKTOP-7RQ494B\HeoSeonJin <sonjin54@naver.com>
2025-01-13 21:48:20 +0900

    회원가입 화면 만들기 & 꾸미기 | 오픈소스 사용하기(CodePen)

--- node/login-lecture/app/src/routes/home/index.js
+++ node/login-lecture/app/src/routes/home/index.js
@@ -7,6 +7,7 @@ const ctrl = require("./home.ctrl");
 
 router.get("/", ctrl.output.home);
 router.get("/login", ctrl.output.login);
+router.get("/register", ctrl.output.register);
 router.post("/login", ctrl.process.login);
 
 module.exports = router; // 외부에서 사용 가능하게 (app.js에서 사용이 가능해야하니까)
