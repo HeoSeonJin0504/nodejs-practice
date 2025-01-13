4f75020ce0e4fd0cd38f24fd98475fe5d1365930
DESKTOP-7RQ494B\HeoSeonJin <sonjin54@naver.com>
2025-01-13 22:37:08 +0900

    회원가입&라우팅 기능 구현 in 서버(서버 재가동시 회원가입한 정보들이 사라짐)

--- node/login-lecture/app/src/public/js/home/register.js
+++ node/login-lecture/app/src/public/js/home/register.js
@@ -3,17 +3,24 @@
 const id = document.querySelector("#id"),
   name = document.querySelector("#name"),
   password = document.querySelector("#password"),
-  confirmPassword = document.querySelector("#confirm-password");
-registerBtn = document.querySelector("#button");
+  confirmPassword = document.querySelector("#confirm-password"),
+  registerBtn = document.querySelector("#button");
 
 registerBtn.addEventListener("click", register);
 
 function register() {
+  if (!id.value) {
+    return alert("아이디를 입력해주세요.");
+  }
+  
+  if (password.value !== confirmPassword.value) {
+    return alert("비밀번호가 일치하지 않습니다.");
+  }
+
   const req = {
     id: id.value,
     name: name.value,
     password: password.value,
-    confirmPassword: confirmPassword.value,
   };
 
   fetch("/register", {
--- node/login-lecture/app/src/routes/home/index.js
+++ node/login-lecture/app/src/routes/home/index.js
@@ -8,6 +8,8 @@ const ctrl = require("./home.ctrl");
 router.get("/", ctrl.output.home);
 router.get("/login", ctrl.output.login);
 router.get("/register", ctrl.output.register);
+
 router.post("/login", ctrl.process.login);
+router.post("/register", ctrl.process.register);
 
 module.exports = router; // 외부에서 사용 가능하게 (app.js에서 사용이 가능해야하니까)
