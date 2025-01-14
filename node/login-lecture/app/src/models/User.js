ff230bae78cfa73aab1eebc7fde80ff61f29a0e3
DESKTOP-7RQ494B\HeoSeonJin <sonjin54@naver.com>
2025-01-14 11:10:14 +0900

    데이터 파일로 관리하기 | fs(파일시스템) | json

--- node/login-lecture/app/src/models/User.js
+++ node/login-lecture/app/src/models/User.js
@@ -9,15 +9,15 @@ class User {
 
   login() {
     const client = this.body; // body에 있는 데이터를 client에 저장
-    const { id, password } = UserStorage.getUserInfo(client.id);
+    console.log(UserStorage.getUserInfo(client.id));
 
-    if (id) {
-      if (id === client.id && password === client.password) {
-        return { success: true };
-      }
-      return { success: false, msg: "비밀번호가 틀렸습니다." };
-    }
-    return { success: false, msg: "존재하지 않는 아이디입니다." };
+    // if (id) {
+    //   if (id === client.id && password === client.password) {
+    //     return { success: true };
+    //   }
+    //   return { success: false, msg: "비밀번호가 틀렸습니다." };
+    // }
+    // return { success: false, msg: "존재하지 않는 아이디입니다." };
   }
 
   register() {
--- node/login-lecture/app/src/models/UserStorage.js
+++ node/login-lecture/app/src/models/UserStorage.js
@@ -1,16 +1,11 @@
 "use strict";
 
+const fs = require("fs"); // 파일 시스템
+
 // UserStorage 클래스 생성 (클래스 안에 객체는 const 안쓴다)
 class UserStorage {
-  static #users = {
-    // static으로 선언하면 클래스명으로 접근 가능, # = private
-    id: ["test", "test1"],
-    password: ["1234", "12345"],
-    name: ["테스트", "테스트1"],
-  };
-
   static getUsers(...fields) {
-    const users = this.#users;
+    // const users = this.#users;
     const newUsers = fields.reduce((newUsers, field) => {
       console.log(newUsers, field);
       if (users.hasOwnProperty(field)) {
@@ -23,18 +18,22 @@ class UserStorage {
   }
 
   static getUserInfo(id) {
-    const users = this.#users;
-    const idx = users.id.indexOf(id);
-    const usersKeys = Object.keys(users); // users의 key값을 배열로 반환 => [id, password, name]
-    const userInfo = usersKeys.reduce((newUser, info) => {
-      newUser[info] = users[info][idx];
-      return newUser;
-    }, {});
-    return userInfo;
+    fs.readFile("./src/databases/users.json", "utf8", (err, data) => {
+      if (err) throw err; // 에러 발생 시 에러 던지기
+      const users = JSON.parse(data);
+      console.log(users);
+      const idx = users.id.indexOf(id);
+      const usersKeys = Object.keys(users); // users의 key값을 배열로 반환 => [id, password, name]
+      const userInfo = usersKeys.reduce((newUser, info) => {
+        newUser[info] = users[info][idx];
+        return newUser;
+      }, {});
+      return userInfo;
+    });
   }
 
   static save(userInfo) {
-    const users = this.#users;
+    // const users = this.#users;
     users.id.push(userInfo.id);
     users.name.push(userInfo.name);
     users.password.push(userInfo.password);
