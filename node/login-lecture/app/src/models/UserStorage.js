234797bcfdb196fdf83f7a5e7b60ca858076d018
DESKTOP-7RQ494B\HeoSeonJin <sonjin54@naver.com>
2025-01-17 16:11:38 +0900

    AWS RDS로 회원가입 구현 | 웹서버와 WAS

--- node/login-lecture/app/src/models/UserStorage.js
+++ node/login-lecture/app/src/models/UserStorage.js
@@ -4,45 +4,34 @@ const db = require("../config/db");
 
 // UserStorage 클래스 생성 (클래스 안에 객체는 const 안쓴다)
 class UserStorage {
-  static #getUserInfo(data, id) {
-    const users = JSON.parse(data);
-    const idx = users.id.indexOf(id);
-    const usersKeys = Object.keys(users); // users의 key값을 배열로 반환 => [id, password, name]
-    const userInfo = usersKeys.reduce((newUser, info) => {
-      newUser[info] = users[info][idx];
-      return newUser;
-    }, {});
-    return userInfo;
-  }
-
-  static #getUsers(data, isAll, fields) {
-    const users = JSON.parse(data);
-    if (isAll) {
-      return users;
-    }
-    const newUsers = fields.reduce((newUsers, field) => {
-      if (users.hasOwnProperty(field)) {
-        // users에 field가 존재하면
-        newUsers[field] = users[field]; // newUsers에 field 추가
-      }
-      return newUsers;
-    }, {});
-    return newUsers;
-  }
-
   static getUsers(isAll, ...fields) {}
 
   static getUserInfo(id) {
     return new Promise((resolve, reject) => {
-      db.query("SELECT * FROM users WHERE id = ?", [id], (err, data) => {
+      const query = "SELECT * FROM users WHERE id = ?;";
+      db.query(query, [id], (err, data) => {
         if (err) {
-          reject(err);
+          reject(`${err}`); // err가 오브젝트이기 때문에 문자열로 변환
         }
         resolve(data[0]);
       });
     });
   }
 
-  static async save(userInfo) {}
+  static async save(userInfo) {
+    return new Promise((resolve, reject) => {
+      const query = "INSERT INTO users(id, name, password) VALUES(?, ?, ?);";
+      db.query(
+        query,
+        [userInfo.id, userInfo.name, userInfo.password],
+        (err) => {
+          if (err) {
+            reject(`${err}`); // err가 오브젝트이기 때문에 문자열로 변환
+          }
+          resolve({ success: true });
+        }
+      );
+    });
+  }
 }
 module.exports = UserStorage;
